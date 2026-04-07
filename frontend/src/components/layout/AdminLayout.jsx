003ebadcbd59df8549ac0b32174bc9e11948d16b
Ly Tinh - 3280 <lytinhqqpy@gmail.com>
2026-04-08 00:06:33 +0700

    fix modal ui and site overflow

--- frontend/src/components/layout/AdminLayout.jsx
+++ frontend/src/components/layout/AdminLayout.jsx
@@ -1,20 +1,21 @@
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 export default function AdminLayout() {
   return (
-    <div className="flex min-h-screen w-screen bg-muted/40">
+    <div className="flex h-screen w-full overflow-hidden bg-muted/40">
 
       {/* Sidebar */}
       <Sidebar />
 
       {/* Main content */}
-      <div className="flex flex-col flex-1 min-w-0">
-
-        <main className="flex-1 p-6 w-full">
-          <Outlet />
-        </main>
-
+      <div className="flex flex-col flex-1 min-w-0 h-full">
+        <ScrollArea className="flex-1 h-full w-full">
+          <main className="p-6 w-full">
+            <Outlet />
+          </main>
+        </ScrollArea>
       </div>
     </div>
   );
